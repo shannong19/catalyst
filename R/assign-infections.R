122cb2fef76af6288dc48803dfe3eddaf9906f4f
shannong19 <sgallagh@andrew.cmu.edu>
2019-05-21 13:48:00 -0400

    completely redid assignment function because I needed to sequentially sample since I was removing samples as I went.  Probs could be a lot faster in rcpp

--- R/assign-infections.R
+++ R/assign-infections.R
@@ -31,26 +31,56 @@ assign_inf_recov_dates <- function(full_df,
 #' @details Requires full_df and inf_df to have an "AGE" column and inf_df must have an I and R column
 assign.byage <- function(full_df,
                          inf_df,
-                         arg_list = list(age_cutoff = 1,
-                                         sequential = FALSE)){
+                         arg_list = list(age_cutoff = 1)){
 
+    stopifnot(nrow(full_df) >= nrow(inf_df)) ## Need to have agent set large enough
     full_df$ind <- 1:nrow(full_df) # Need to maintain original order
-    assigned_df <- plyr::ddply(.data = inf_df, .var = c("AGE"),
-                               .fun = function(df){
-                                   age <- unique(df$AGE)
-                                   lower <- age - arg_list$age_cutoff
-                                   upper <- age + arg_list$age_cutoff
-                                   sub_inds <- which(full_df$Age >= lower |
-                                                     full_df$Age <= upper)
-                                   n <- nrow(df)
-                                   sampled_inds <- sample(sub_inds, size = n)
-                                   new_sub_df <- data.frame(ind = sampled_inds,
-                                                            I = df$I,
-                                                            R = df$R)
-                                   return(new_sub_df)
+    inf_df$ebola_id <- 1:nrow(inf_df)
 
-                          })
+    ## Split by ages
+    random_ages <- sample(unique(inf_df$AGE))
+    sub_df <- full_df  ## Initially we sample indices from full data frame
+    new_sub_list <- vector(mode = "list", length = length(unique(inf_df$AGE)))
+    print(random_ages)
+    for(ii in 1:length(random_ages)){
+        ## Subset infection DF
+        age <- random_ages[ii]
+        if(is.na(age)){
+            df <- inf_df[which(is.na(inf_df$AGE)),]
+            sub_inds <- NULL
+        } else {
+            df <- inf_df[which(inf_df$AGE == age), ]
+            lower <- age - arg_list$age_cutoff
+            upper <- age + arg_list$age_cutoff
+            sub_inds <- sub_df$ind[which(sub_df$AGE >= lower &
+                                         sub_df$AGE <= upper)]
+        }
+        N <- nrow(df)
+        
+     #   print(paste("Age:", age, "; N", N))
+       
+        if(length(sub_inds) == 1){
+            sampled_inds <- sub_inds
+        }  else if(length(sub_inds) < N | is.na(age)){
+            ## If there are not enough subjects to sample from, expand to whole (remaining) DF
+            sub_inds <- sub_df$ind
+            sampled_inds <- base::sample(x = sub_inds, size = N)
+        } else {
+            sampled_inds <- base::sample(x = sub_inds, size = N)
+        }
+        new_sub_list[[ii]] <- data.frame(ind = sampled_inds,
+                                         I = df$I,
+                                         R = df$R,
+                                         AGE_inf = df$AGE,
+                                         ebola_id = df$ebola_id)
+        ## Get rid of sampled inds, cannot be picked again
+        sub_df <- sub_df[ - which(sub_df$ind %in% sampled_inds), ]
+    }
+    assigned_df <- do.call('rbind', new_sub_list)
+    print(length(unique(assigned_df$ind)))
+    stopifnot(length(unique(assigned_df$ind)) == nrow(inf_df))
     new_df <- dplyr::left_join(full_df, assigned_df, by = "ind")
+    new_df <- new_df[, - which(colnames(new_df) %in% "ind")]
     
     return(new_df)
 
--- tests/testthat/test-assign-infections.R
+++ tests/testthat/test-assign-infections.R
@@ -2,7 +2,30 @@
 context("Testing assign_inf_recov_dates functions")
 
 test_that("assign.byage", {
+    
+    full_df <- data.frame(AGE = c(2, 40, 12, 85, 85),
+                          banana = c(T, T, T, F, T))
+    inf_df <- data.frame(AGE = c(39, 2, 13, 85, 85),
+                         I = c(1, 13, 2, 5, 20),
+                         R = c(9, 15, 5, 14, 22))
+    arg_list <- list(age_cutoff = 1, sequential = FALSE)
 
-    ## TODO
+    out <- assign.byage(full_df = full_df,
+                        inf_df = inf_df,
+                        arg_list = arg_list)
 
-    })
+    expect_equal(dim(out), c(5, 4))
+    
+    ## Test where there aren't enough matches
+    full_df <- data.frame(AGE = c(2, 40, 12, 85, 19),
+                          banana = c(T, T, T, F, T))
+    inf_df <- data.frame(AGE = c(39, 2, 13, 85, 85),
+                         I = c(1, 13, 2, 5, 20),
+                         R = c(9, 15, 5, 14, 22))
+    arg_list <- list(age_cutoff = 1, sequential = FALSE)
+    out <- assign.byage(full_df = full_df,
+                        inf_df = inf_df,
+                        arg_list = arg_list)
+
+    expect_equal(dim(out), c(5, 4))
+})
