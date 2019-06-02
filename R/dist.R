4435b0d6a9353e0123adf7c2ef9d6baf0dae115f
shannong19 <sgallagh@andrew.cmu.edu>
2019-06-02 18:13:20 -0400

    dist functions

--- R/dist.R
+++ R/dist.R
@@ -10,3 +10,26 @@ euc_dist <- function(m){
     sq <- rowSums(m*m)
     sqrt(outer(sq,sq,"+") - 2*mtm)
 } 
+
+
+mstat <- function(JT, J0, dist = dist_haversine_rcpp){
+    JT <- as.matrix(JT)
+    J0 <- as.matrix(J0)
+    min_dists <- apply(JT, 1, mstat_inner, J0 = J0,
+                       dist = dist)
+
+}
+
+
+mstat_inner <- function(x, J0, dist = dist_haversine_rcpp){
+    dists <- apply(J0, 1, function(y){
+        xlon <- x[1]
+        xlat <- x[2]
+        ylon <- y[1]
+        ylat <- y[2]
+        dist <- dist(xlon, xlat, ylon, ylat)
+        return(dist)
+
+    })
+    return(min(dists))
+}
